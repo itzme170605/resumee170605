e08c6038015d8aa79cc98cafd8eb714c4be6b544
itzme <121313000+itzme170605@users.noreply.github.com>
2023-10-03 01:36:22 -0400

    Add files via upload
    Just added files

---
+++ script.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".multiple-text", {
+  strings: [
+    " a Student",
+    " a Programmer",
+    " a Developer",
+    " an Animator",
+    " an Artist",
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 5,
+  loop: true,
+});
